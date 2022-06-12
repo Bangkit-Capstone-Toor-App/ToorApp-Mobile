adbe7ce4ea5e7af8876e9c69a159dda14b4b0431
nadhofaarief <nadhofa40@gmail.com>
2022-06-13 01:42:53 +0700

    Detail Page

---
+++ app/src/main/java/com/kamil/toorapp_mobile/DetailPlace.kt
@@ -0,0 +1,11 @@
+package com.kamil.toorapp_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailPlace : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_place)
+    }
+}
