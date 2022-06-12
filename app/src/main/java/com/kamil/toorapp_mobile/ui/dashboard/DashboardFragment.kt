7570f187052986d8be83f708b7b63867a1682faf
nadhofaarief <nadhofa40@gmail.com>
2022-06-13 00:30:08 +0700

    Recycler View Home Page

--- app/src/main/java/com/kamil/toorapp_mobile/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/kamil/toorapp_mobile/ui/dashboard/DashboardFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.kamil.toorapp_mobile.databinding.FragmentDashboardBinding
+import com.kamil.toorapp_mobile.databinding.FragmentLocationBinding
 
 class DashboardFragment : Fragment() {
 
-    private var _binding: FragmentDashboardBinding? = null
+    private var _binding: FragmentLocationBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,8 @@ class DashboardFragment : Fragment() {
         val dashboardViewModel =
             ViewModelProvider(this).get(DashboardViewModel::class.java)
 
-        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
+        _binding = FragmentLocationBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
